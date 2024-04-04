3f5b090314504122f944c6e6c603a08f1955f8d1
Andrej Stjepanović <stjepanoviccc2021@gmail.com>
2024-04-04 23:29:54 +0200

    Feature repository implementation (#4)
    config groups repo implementation

---
+++ repositories/configGroup_inmem.go
@@ -0,0 +1,51 @@
+package repositories
+
+import (
+	"ars_projekat/model"
+	"errors"
+	"fmt"
+)
+
+type ConfigGroupInMemoryRepository struct {
+	configGroups map[string]model.ConfigurationGroup
+}
+
+func (c ConfigGroupInMemoryRepository) Add(configGroup model.ConfigurationGroup) {
+	key := fmt.Sprintf("%s/%d", configGroup.Name, configGroup.Version)
+	c.configGroups[key] = configGroup
+}
+
+func (c ConfigGroupInMemoryRepository) Get(name string, version int) (model.ConfigurationGroup, error) {
+	key := fmt.Sprintf("%s/%d", name, version)
+	configGroup, ok := c.configGroups[key]
+	if !ok {
+		return model.ConfigurationGroup{}, errors.New("config not found")
+	}
+	return configGroup, nil
+}
+
+func (c ConfigGroupInMemoryRepository) Delete(configGroup model.ConfigurationGroup) error {
+	key := fmt.Sprintf("%s%d", configGroup.Name, configGroup.Version)
+	_, ok := c.configGroups[key]
+	if !ok {
+		return errors.New("config not found")
+	}
+	delete(c.configGroups, key)
+	return nil
+}
+
+func (c ConfigGroupInMemoryRepository) Update(configGroup model.ConfigurationGroup) error {
+	key := fmt.Sprintf("%s%d", configGroup.Name, configGroup.Version)
+	_, ok := c.configGroups[key]
+	if !ok {
+		return errors.New("config not found")
+	}
+	c.configGroups[key] = configGroup
+	return nil
+}
+
+func NewConfigGroupInMemoryRepository() model.ConfigurationGroupRepository {
+	return ConfigGroupInMemoryRepository{
+		configGroups: make(map[string]model.ConfigurationGroup),
+	}
+}
